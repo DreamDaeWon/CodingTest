9293e141b49dbe8329c11d0612be49aacc0b3f22
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2024-07-20 00:34:34 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string strSentence{};
+
+	cin >> strSentence;
+
+	cout << strSentence.length() << endl;
+
+	return 0;
+}
