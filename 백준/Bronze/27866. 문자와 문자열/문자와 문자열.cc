87bda88a0ead293d900c10f869711185d7f7b7c4
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2025-01-03 14:15:57 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string All;
+
+	cin >> All;
+
+	int A{};
+
+	cin >> A;
+
+	cout << All[A-1] << endl;
+
+
+
+	return 0;
+}
