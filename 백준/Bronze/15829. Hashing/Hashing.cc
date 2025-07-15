20584ee6ae5e8a4965125c38ec99b2fa904418b9
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2025-07-15 16:41:04 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,37 +1,56 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std;
 
+
+size_t my_pow(size_t num1, size_t num2)
+{
+	size_t Fin{1};
+	for (size_t i = 0; i < num2; ++i)
+	{
+		Fin *= num1;
+
+		if (Fin >= 1234567891)
+		{
+			Fin %= 1234567891;
+		}
+	}
+
+	return Fin;
+}
+
+
+
 int main()
 {
-	int AllSize{};
+	size_t Size{};
+	
+	cin >> Size;
 
-	cin >> AllSize;
-
-	int Num{};
-
-	int Input{};
-
-	int Result{};
-
-	char* Str = new char[AllSize+1];
+	string Str{};
 
 	cin >> Str;
 
-	for (int i = 0; i < AllSize; ++i)
+
+	size_t iC{};
+
+	size_t Fin{}; // 결과
+
+	for (size_t i = 0 ; i < Size; ++i)
 	{
-		Num = Str[i] - 96;
-		Result += Num * pow(31, i);
+		iC = (char)Str[i] - 'a' + 1;
+		
+		Fin += iC * my_pow(31, i);
+
+		if (Fin >= 1234567891)
+		{
+			Fin %= 1234567891;
+		}
+
 	}
 
-	Result = Result % 1234567891;
-
-
-	cout << Result;
-
-
-	delete[] Str;
-	Str = nullptr;
+	cout << Fin;
 
 	return 0;
 }
