288b62a2f10cca5ec307c775e32ea3377bf86ac3
DreamDaeWon <142958544+DreamDaeWon@users.noreply.github.com>
2026-03-09 10:36:01 +0900

    [Bronze II] Title: 하얀 칸, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,14 @@
+
+flag = False
+
+count = 0
+
+for i in range(8):
+    arr = input()
+    flag = not flag
+    for j in range(8):
+        if flag and arr[j] == 'F':
+            count += 1
+        flag = not flag
+
+print(count)
